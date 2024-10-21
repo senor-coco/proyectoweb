d814b7ee380c20e2ef08e0fd7de51839bdc514bf
coco <130810567+senor-coco@users.noreply.github.com>
2024-10-21 03:03:42 -0600

    viajes

--- vista/index.php
+++ vista/index.php
@@ -1,39 +1,46 @@
 <?php require_once("layout/header.php");?>
-<div class="container text-center">
+
+<div class="container text-center" style="margin-top: 50px;"> <!-- Añadimos margen superior -->
   <div class="row">
+    <!-- Caribe y Pacífico -->
     <div class="col">
-    <div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-</div>
+      <div class="card" style="width: 18rem;">
+        <img src="vista/imagenes/jpg/enelcaibeypasifico.jpg" class="card-img-top" alt="Caribe y Pacífico">
+        <div class="card-body">
+          <h5 class="card-title">Caribe y Pacífico</h5>
+          <h6 class="card-subtitle mb-2 text-body-secondary">Una experiencia única</h6>
+          <p class="card-text">Descubre la belleza del Caribe y el Pacífico.</p>
+          <a href="#" class="card-link">Ver más</a>
+        </div>
+      </div>
     </div>
+
+    <!-- Huatulco -->
     <div class="col">
-    <div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-</div>
+      <div class="card" style="width: 18rem;">
+        <img src="vista/imagenes/jpg/huatulco.jpg" class="card-img-top" alt="Huatulco">
+        <div class="card-body">
+          <h5 class="card-title">Huatulco</h5>
+          <h6 class="card-subtitle mb-2 text-body-secondary">El paraíso en la tierra</h6>
+          <p class="card-text">Explora las bahías de Huatulco y su naturaleza.</p>
+          <a href="#" class="card-link">Ver más</a>
+        </div>
+      </div>
     </div>
+
+    <!-- Cabo San Lucas -->
     <div class="col">
-    <div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-</div>
+      <div class="card" style="width: 18rem;">
+        <img src="vista/imagenes/jpg/cabosanlucas.jpg" class="card-img-top" alt="Cabo San Lucas">
+        <div class="card-body">
+          <h5 class="card-title">Cabo San Lucas</h5>
+          <h6 class="card-subtitle mb-2 text-body-secondary">Un destino de lujo</h6>
+          <p class="card-text">Vive la experiencia de Cabo San Lucas.</p>
+          <a href="#" class="card-link">Ver más</a>
+        </div>
+      </div>
     </div>
   </div>
 </div>
+
 <?php require_once("layout/footer.php");?>
