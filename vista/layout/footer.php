6f0b5888cf82532917cf2a435079e86ab70f40cc
coco <130810567+senor-coco@users.noreply.github.com>
2024-10-21 17:02:03 -0600

    stylesajustes

--- vista/layout/footer.php
+++ vista/layout/footer.php
@@ -1,7 +1,4 @@
 <footer>
-    <p>Redes Sociales</p>
-    <p>© 2024 Agencia de Viajes. Todos los derechos reservados.</p>
-
+    <p class="footer-text">Redes Sociales</p>
+    <p class="footer-text">© 2024 Agencia de Viajes. Todos los derechos reservados.</p>
 </footer>
-</body>
-</html>
